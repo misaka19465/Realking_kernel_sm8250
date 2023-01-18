5497a972e527b316eecd2b26fe686ebfb61e9e01
Sergey Senozhatsky <senozhatsky@chromium.org>
2023-01-18 09:52:09 +0900

    BACKPORT: zsmalloc: make zspage chain size configurable
    Remove hard coded limit on the maximum number of physical pages
    per-zspage.
    
    This will allow tuning of zsmalloc pool as zspage chain size changes
    `pages per-zspage` and `objects per-zspage` characteristics of size
    classes which also affects size classes clustering (the way size classes
    are merged).
    
    Link: https://lkml.kernel.org/r/20230118005210.2814763-4-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -67,17 +67,6 @@
  */
 #define ZS_ALIGN		8
 
-/*
- * A single 'zspage' is composed of up to 2^N discontiguous 0-order (single)
- * pages. ZS_MAX_ZSPAGE_ORDER defines upper limit on N.
- */
-#if defined(OPLUS_FEATURE_ZRAM_OPT) && defined(CONFIG_OPLUS_ZRAM_OPT)
-#define ZS_MAX_ZSPAGE_ORDER 3
-#else
-#define ZS_MAX_ZSPAGE_ORDER 2
-#endif /*OPLUS_FEATURE_ZRAM_OPT*/
-#define ZS_MAX_PAGES_PER_ZSPAGE (_AC(1, UL) << ZS_MAX_ZSPAGE_ORDER)
-
 #define ZS_HANDLE_SIZE (sizeof(unsigned long))
 
 /*
@@ -127,13 +116,16 @@
 #define FULLNESS_BITS	2
 #define CLASS_BITS	8
 #if defined(OPLUS_FEATURE_ZRAM_OPT) && defined(CONFIG_OPLUS_ZRAM_OPT)
-#define ISOLATED_BITS	(ZS_MAX_ZSPAGE_ORDER+1)
+#define ISOLATED_BITS	5
 #else
 #define ISOLATED_BITS	3
 #endif /*OPLUS_FEATURE_ZRAM_OPT*/
 #define MAGIC_VAL_BITS	8
 
 #define MAX(a, b) ((a) >= (b) ? (a) : (b))
+
+#define ZS_MAX_PAGES_PER_ZSPAGE	(_AC(CONFIG_ZSMALLOC_CHAIN_SIZE, UL))
+
 /* ZS_MIN_ALLOC_SIZE must be multiple of ZS_ALIGN */
 #define ZS_MIN_ALLOC_SIZE \
 	MAX(32, (ZS_MAX_PAGES_PER_ZSPAGE << PAGE_SHIFT >> OBJ_INDEX_BITS))
